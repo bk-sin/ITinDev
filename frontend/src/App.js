446104934982d5f592b582be399684431f5f1ab1
bk-sin <beckasin.mc@gmail.com>
2021-12-23 17:38:08 -0300

    probando tinder swipe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
+import TinderCard from "react-tinder-card"
+import "./css.css"
+
 function App() {
+  const onSwipe = (direction) => {
+    console.log("You swiped: " + direction)
+  }
+
+  const onCardLeftScreen = (myIdentifier) => {
+    console.log(myIdentifier + " left the screen")
+  }
   return (
-    <div>
+    <div className="padre">
       <h1>ITinDev</h1>
+      <TinderCard
+        className="swipe"
+        onSwipe={onSwipe}
+        onCardLeftScreen={() => onCardLeftScreen("fooBar")}
+        preventSwipe={["up", "down"]}
+      >
+        <img src="https://preview.redd.it/8zr60tk7ra781.jpg?width=640&crop=smart&auto=webp&s=72838e77707d05157c424377386f866738c2147c" />
+      </TinderCard>
     </div>
   )
 }
