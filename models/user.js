5dceade9b20369ca1ead9e9e18246ef9ed0e0388
bk-sin <beckasin.mc@gmail.com>
2021-12-24 16:03:02 -0300

    fix model

--- models/user.js
+++ models/user.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-        name:{type:String, required:true},
-        lastName:{type:String, required:true},
-        country:{type:String, requiere:true},
-        email:{type:String, required:true},
-        password:{type:String, required:true},
-        gender:{type:String, required:true},
-        image:String,
-        google: {type: Boolean, default: false} 
+  name: {type: String, required: true},
+  lastName: {type: String, required: true},
+  country: {type: String, requiere: true},
+  email: {type: String, required: true},
+  password: {type: String, required: true},
+  gender: {type: String, required: true},
+  image: {type: String},
+  google: {type: Boolean, default: false},
 })
 
-const User= mongoose.model('persona', userSchema)
+const User = mongoose.model("user", userSchema)
 
-module.exports = User;
+module.exports = User
