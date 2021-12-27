625adea8be290baa85301656d4ba293f5c329a02
lucas-romer <romerolucas89@gmail.com>
2021-12-27 09:39:50 -0300

    prueba

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -8,7 +8,7 @@ function Main() {
         <div className='container_principal'>
             <div className='container_cuenta'>
                 <h1 className='titulo_principal'>¿Estás list@ para codear tu corazón?</h1>
-                <button className='btn-grad'>Crear cuenta</button>
+                <button className='btn-grad'>Crear cuenta de citas</button>
             </div>
 
         </div>
