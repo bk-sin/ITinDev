712b4452295b811633c2b4d37cc505d2a1b9dfc6
lucas-romer <romerolucas89@gmail.com>
2021-12-27 11:37:31 -0300

    se agregan rutas

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -4,7 +4,9 @@ import Admin from "../components/Admin"
 import TestPadre from "../components/TestPadre"
 import {connect} from "react-redux"
 import authAction from "../redux/actions/authActions"
-
+import Main from "../components/Main"
+import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
 import {Routes, Route, Navigate} from "react-router-dom"
 
 function RoutesManager(props) {
@@ -21,7 +23,9 @@ function RoutesManager(props) {
           !props.user.name ? <Navigate replace to="/signup" /> : <TestPadre />
         }
       />
-
+      <Route
+      path="/"
+      element={<Main/>}/>
       <Route
         path="/admin"
         element={props.user.admin ? <Admin /> : <Navigate replace to="/test" />}
