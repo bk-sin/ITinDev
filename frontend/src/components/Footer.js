c2c133bed734562ca0d3590b6aa106b6e1bda450
bk-sin <beckasin.mc@gmail.com>
2021-12-30 04:27:24 -0300

    target blank

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Nav } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import React from "react"
+import {Nav} from "react-bootstrap"
+import {Link} from "react-router-dom"
 
 function Footer() {
-    return (
-        <footer>
-            <div className="footer-nav" variant="dark">
-                <div className="icons-container">
-                    <Nav.Link href="https://github.com/bk-sin/ITinDev" className="link-social"><i className="fab fa-github" id="github"></i></Nav.Link>
-                </div>
-            <p>&copy; ItinDev | All Rights Reserved</p>
-            </div>
-        </footer>
-    )
+  return (
+    <footer>
+      <div className="footer-nav" variant="dark">
+        <div className="icons-container">
+          <Nav.Link
+            href="https://github.com/bk-sin/ITinDev"
+            target="blank"
+            className="link-social"
+          >
+            <i className="fab fa-github" id="github"></i>
+          </Nav.Link>
+        </div>
+        <p>&copy; ItinDev | All Rights Reserved</p>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
