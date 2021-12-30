f281ecf3f64a81ae7827d09c378b72fee506d2a9
bk-sin <beckasin.mc@gmail.com>
2021-12-30 00:18:13 -0300

    make it better

--- server.js
+++ server.js
@@ -1,71 +1,73 @@
-require("dotenv").config();
-const passport = require("passport");
-const express = require("express");
-const cors = require("cors");
-const Router = require("./routes/routes");
+require("dotenv").config()
+const passport = require("passport")
+const express = require("express")
+const cors = require("cors")
+const Router = require("./routes/routes")
 
-require("./config/database");
+require("./config/database")
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(passport.initialize());
-app.use("/api", Router);
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(passport.initialize())
+app.use("/api", Router)
 
-let server = app.listen("4000", () =>
-  console.log("Server listening on port 4000")
-);
+let server = app.listen(
+  process.env.PORT || "4000",
+  process.env.HOST || "0.0.0.0",
+  () => console.log(`Server listening port: ${process.env.PORT || "4000"}`)
+)
 
-const { Server } = require("socket.io");
-const io = new Server(server, { cors: { origin: "*" } });
+const {Server} = require("socket.io")
+const io = new Server(server, {cors: {origin: "*"}})
 /* io.on("connect", (socket) => {
   console.log("Socket Connected"); 
 });*/
 
-let users = [];
+let users = []
 
 const addUser = (userId, socketId) => {
   !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
+    users.push({userId, socketId})
   for (var i = users.length - 1; i >= 0; --i) {
     if (users[i].userId == null) {
-      users.splice(i, 1);
+      users.splice(i, 1)
     }
   }
-};
+}
 
 const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
-};
+  users = users.filter((user) => user.socketId !== socketId)
+}
 
 const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
-};
+  return users.find((user) => user.userId === userId)
+}
 
 io.on("connection", (socket) => {
   //when ceonnect
-  console.log("a user connected.");
+  console.log("a user connected.")
 
   //take userId and socketId from user
   socket.on("addUser", (userId) => {
-    addUser(userId, socket.id);
-    io.emit("getUsers", users);
-  });
+    addUser(userId, socket.id)
+    io.emit("getUsers", users)
+  })
 
   //send and get message
-  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-    const user = getUser(receiverId);
+  socket.on("sendMessage", ({senderId, receiverId, text}) => {
+    const user = getUser(receiverId)
     users.find((receiver) => receiver === user) &&
       io.to(user.socketId).emit("getMessage", {
         senderId,
         text,
-      });
-  });
+      })
+  })
 
   //when disconnect
   socket.on("disconnect", () => {
-    console.log("a user disconnected!");
-    removeUser(socket.id);
-    io.emit("getUsers", users);
-  });
-});
+    console.log("a user disconnected!")
+    removeUser(socket.id)
+    io.emit("getUsers", users)
+  })
+})
