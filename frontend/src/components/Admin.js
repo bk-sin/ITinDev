0a81f979432ebea2357de2be5cdd365a502f5b7d
silc22 <silcerquatti@gmail.com>
2021-12-30 02:17:30 -0300

    feat: resposive & style admin componente

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -23,45 +23,49 @@ function Admin(props) {
   }, [props.personas])
 
   return (
-    <>
+      <>
       {!props.user.name ? (
         <h1>Loading...</h1>
       ) : !props.user.admin ? (
         navigate("/", {replace: true})
       ) : (
-        <>
-          <h1>Admin Panel</h1>
-          <h2>Welcome {props.user.name}</h2>
-          <ul>
-            {props.personas !== "Oops!error" &&
-              props.auxiliar.map((character, index) => (
-                <User
-                  key={index}
-                  index={index}
-                  user={character}
-                  deletePeople={props.deletePeople}
-                  editPeople={props.editPeople}
-                  getAllUsers={props.getAllUsers}
-                  banPeople={props.banPeople}
+        <div className="contenedor-admin-page">
+            <div className="usuarios-admin-filters">
+              <h3>Hola {props.user.name}! 👋</h3>
+              <h2>Administrador de usuarios:</h2>
+              <div className="filter-conteiner">
+                <button
+                  onClick={() => {
+                    props.setPersonas(ascdes)
+                    props.filterPeople()
+                    setascdes(ascdes === "ASC" ? "DES" : "ASC")
+                  }}
+                >
+                  Ordenar alfabéticamente
+                </button>
+                <input
+                  className="SearchInput"
+                  onChange={(e) => props.filterPeople(e.target.value)}
+                  type="text"
+                  placeholder="Busca un usuario"
                 />
-              ))}
-          </ul>
-          <button
-            onClick={() => {
-              props.setPersonas(ascdes)
-              props.filterPeople()
-              setascdes(ascdes === "ASC" ? "DES" : "ASC")
-            }}
-          >
-            Alfabetico
-          </button>
-          <input
-            className="SearchInput"
-            onChange={(e) => props.filterPeople(e.target.value)}
-            type="text"
-            placeholder="Search a City"
-          />
-        </>
+              </div>
+            </div>
+            <div className="usuarios-admin-conteiner">
+              {props.personas !== "Oops!error" &&
+                props.auxiliar.map((character, index) => (
+                  <User
+                    key={index}
+                    index={index}
+                    user={character}
+                    deletePeople={props.deletePeople}
+                    editPeople={props.editPeople}
+                    getAllUsers={props.getAllUsers}
+                    banPeople={props.banPeople}
+                  />
+                ))}
+            </div>
+        </div>
       )}
     </>
   )
--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -16,8 +16,8 @@ export default function Admin(props) {
   function handleEdit(e) {
     e.preventDefault()
     swal({
-      title: "Are you sure?",
-      text: "Once deleted, you will not be able to recover this imaginary file!",
+      title: "Estás seguro?",
+      text: "Una vez eliminado, no podras recuperarlo!",
       icon: "warning",
       buttons: true,
       dangerMode: true,
@@ -33,151 +33,158 @@ export default function Admin(props) {
           gender: gender.current.value,
           description: description.current.value,
         })
-        swal("Poof! Your imaginary file has been deleted!", {
+        swal("Poof! El usuario fue eliminado!", {
           icon: "success",
         })
       } else {
-        swal("Your imaginary file is safe!")
+        swal("El usuario ha sido salvado!")
       }
     })
   }
+
+
   return (
-    <li key={props.index}>
+    <>
+    <div key={props.index} className="usuarios-admin">
+      {props.user.admin && <span style={{color: "red"}}>ADMIN</span>}
+      {props.user.banned && <span style={{color: "red"}}>BANNED</span>}
       <img
         src={props.user.image}
         style={{width: "50px", height: "50px", objectFit: "cover"}}
         alt="admin user pic"
       />
       {props.user.name} {props.user.lastName}{" "}
-      {props.user.admin && <span style={{color: "red"}}>ADMIN</span>}
-      {props.user.banned && <span style={{color: "red"}}>BANNED</span>}
-      <button
-        onClick={() => {
-          swal({
-            title: "Are you sure?",
-            text: "Once deleted, you will not be able to recover this imaginary file!",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-          }).then((willDelete) => {
-            if (willDelete) {
-              props.deletePeople(props.user._id, "DEL")
-              swal("Poof! Your imaginary file has been deleted!", {
-                icon: "success",
+      <div className="button-admin-conteiner">
+          <button
+            onClick={() => {
+              swal({
+                title: "Estás seguro?",
+                text: "Una vez eliminado, no podras recuperarlo! 😰",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+              }).then((willDelete) => {
+                if (willDelete) {
+                  props.deletePeople(props.user._id, "DEL")
+                  swal("Poof! El usuario fue eliminado!", {
+                    icon: "success",
+                  })
+                } else {
+                  swal("El usuario ha sido salvado! 😅")
+                }
               })
-            } else {
-              swal("Your imaginary file is safe!")
-            }
-          })
-        }}
-      >
-        DELETE USER!
-      </button>
-      <button
-        onClick={() => {
-          setEdit(!edit)
-        }}
-      >
-        EDIT USER!
-      </button>
-      {edit && (
-        <form onSubmit={handleEdit}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            className="inputs-btn"
-            ref={name}
-            required
-            defaultValue={props.user.name}
-            minLength="3"
-            maxLength="20"
-          ></input>
-          <label htmlFor="lastname">Lastname</label>
-          <input
-            type="text"
-            id="lastname"
-            className="inputs-btn"
-            ref={lastname}
-            required
-            defaultValue={props.user.lastName}
-            minLength="3"
-            maxLength="20"
-          ></input>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            className="inputs-btn"
-            ref={email}
-            defaultValue={props.user.email}
-            required
-          ></input>
-
-          <label htmlFor="photo">Photo</label>
-          <input
-            type="string"
-            id="photo"
-            className="btn-signup"
-            required
-            defaultValue={props.user.image}
-            ref={photo}
-          ></input>
-          <label htmlFor="age">Age</label>
-          <input
-            type="number"
-            id="age"
-            className="inputs-btn"
-            required
-            defaultValue={props.user.age}
-            ref={age}
-          ></input>
-          <label htmlFor="gender">Gender</label>
-          <select
-            type="string"
-            id="gender"
-            className="inputs-btn"
-            required
-            defaultValue={props.user.gender}
-            ref={gender}
+            }}
           >
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
-          <label htmlFor="country">Country</label>
-          <select
-            type="text"
-            id="country"
-            className="inputs-btn"
-            defaultValue={props.user.country}
-            ref={country}
+            🗑️
+          </button>
+          <button
+            onClick={() => {
+              setEdit(!edit)
+            }}
           >
-            <option value="Argentina">Argentina</option>
-            <option value="Bolivia">Bolivia</option>
-            <option value="Paraguay">Paraguay</option>
-            <option value="Brasil">Brasil</option>
-            <option value="Uruguay">Uruguay</option>
-            <option value="Chile">Chile</option>
-            <option value="Ecuador">Ecuador</option>
-            <option value="Peru">Peru</option>
-          </select>
-          <textarea
-            id="description"
-            className="inputs-btn"
-            required
-            defaultValue={props.user.description}
-            ref={description}
-          ></textarea>
-          <input type="submit" value="Submit" className="btn-submit" />
-        </form>
-      )}
-      <button
-        onClick={() => {
-          props.banPeople(props.user._id)
-        }}
-      >
-        BAN
-      </button>
-    </li>
+            🖊️
+          </button>
+          <button
+            onClick={() => {
+              props.banPeople(props.user._id)
+            }}
+          >
+            ⛔
+          </button>
+          </div>
+    </div>
+        <div className="form-admin-conteiner">
+        {edit && (
+          <form onSubmit={handleEdit}>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              className="inputs-btn"
+              ref={name}
+              required
+              defaultValue={props.user.name}
+              minLength="3"
+              maxLength="20"
+            ></input>
+            <label htmlFor="lastname">Lastname</label>
+            <input
+              type="text"
+              id="lastname"
+              className="inputs-btn"
+              ref={lastname}
+              required
+              defaultValue={props.user.lastName}
+              minLength="3"
+              maxLength="20"
+            ></input>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              className="inputs-btn"
+              ref={email}
+              defaultValue={props.user.email}
+              required
+            ></input>
+            <label htmlFor="photo">Photo</label>
+            <input
+              type="string"
+              id="photo"
+              className="btn-signup"
+              required
+              defaultValue={props.user.image}
+              ref={photo}
+            ></input>
+            <label htmlFor="age">Age</label>
+            <input
+              type="number"
+              id="age"
+              className="inputs-btn"
+              required
+              defaultValue={props.user.age}
+              ref={age}
+            ></input>
+            <label htmlFor="gender">Gender</label>
+            <select
+              type="string"
+              id="gender"
+              className="inputs-btn"
+              required
+              defaultValue={props.user.gender}
+              ref={gender}
+            >
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+            <label htmlFor="country">Country</label>
+            <select
+              type="text"
+              id="country"
+              className="inputs-btn"
+              defaultValue={props.user.country}
+              ref={country}
+            >
+              <option value="Argentina">Argentina</option>
+              <option value="Bolivia">Bolivia</option>
+              <option value="Paraguay">Paraguay</option>
+              <option value="Brasil">Brasil</option>
+              <option value="Uruguay">Uruguay</option>
+              <option value="Chile">Chile</option>
+              <option value="Ecuador">Ecuador</option>
+              <option value="Peru">Peru</option>
+            </select>
+            <textarea
+              id="description"
+              className="inputs-btn"
+              required
+              defaultValue={props.user.description}
+              ref={description}
+            ></textarea>
+            <input type="submit" value="Submit" className="btn-submit" />
+          </form>
+        )}
+    </div>
+    </>
   )
 }
