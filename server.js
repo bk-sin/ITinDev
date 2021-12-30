32bbc15a701777e3560e33eafacb51dd8ddc0594
bk-sin <beckasin.mc@gmail.com>
2021-12-30 00:48:01 -0300

    heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use("/api", Router)
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"))
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/client/build/index.html"))
   })
 }
